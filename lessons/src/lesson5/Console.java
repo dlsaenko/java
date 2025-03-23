1c6bc62de46e5a6246f8e0757a42e8499cf1630d
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-23 14:30:36 +0300

    add Console class

---
+++ lessons/src/lesson5/Console.java
@@ -0,0 +1,17 @@
+package lesson5;
+
+public class Console {
+    public static void main(String[] args) {
+
+        //sout
+        System.out.println("Hello"); //String
+        System.out.println(1);       //int
+        System.out.println(1 + 2);   //int
+        System.out.println("1 + 2"); //String
+
+        System.out.print("Hello");
+        System.out.print(4);
+        System.out.print(3 + 3);
+        System.out.print("1 + 2");
+    }
+}
