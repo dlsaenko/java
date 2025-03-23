37e4864de45cb3a2c1bc394a29a7a1eb454950d8
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-23 14:58:44 +0300

    add Variables class for lesson7

---
+++ lessons/src/L007/Variables/Variables.java
@@ -0,0 +1,12 @@
+package L007.Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        var x = 6;
+        System.out.println(x);
+
+        var y = 8;
+        System.out.println(y);
+    }
+}
