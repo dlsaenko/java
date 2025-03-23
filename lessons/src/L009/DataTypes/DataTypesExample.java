b8884c132bc9c48c3605e6e74379ec0638d7c609
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-23 15:35:27 +0300

    add DataTypes class

---
+++ lessons/src/L009/DataTypes/DataTypesExample.java
@@ -0,0 +1,23 @@
+package L009.DataTypes;
+
+public class DataTypesExample {
+    public static void main(String[] args) {
+
+        String myString = "Hello";
+        System.out.println(myString);
+
+        int myInt = 10;
+        System.out.println(myInt);
+
+        char myChar = 'a';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+        boolean a = 1 > 2;
+        System.out.println(a);
+
+        double myDouble = 1.25;
+        System.out.println(myDouble);
+    }
+}
