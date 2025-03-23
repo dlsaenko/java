ff21fb993baa81c70a71bf7e2df3b2392afa87a5
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-23 15:11:40 +0300

    add Comments class

---
+++ lessons/src/L008/Comments/Comments.java
@@ -0,0 +1,18 @@
+package L008.Comments;
+
+public class Comments {
+    public static void main(String[] args) {
+
+        // 1. It's a one-line comment
+
+        /* 2. This is a
+         *     multi-line
+         *     comment
+         */
+
+        /** 3. This is a javadoc comment
+         * @author Danil Saenko
+         * @version 1.0
+         */
+    }
+}
