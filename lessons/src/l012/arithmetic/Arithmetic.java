2f52bafbd52b3703e3fcf527dd5381243bc2cb94
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-24 23:50:50 +0300

    add Arithmetic class

---
+++ lessons/src/l012/arithmetic/Arithmetic.java
@@ -0,0 +1,15 @@
+package l012.arithmetic;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
