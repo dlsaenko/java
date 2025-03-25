3b9b5eba9df122a13542bd8ef8300049036430f8
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-25 12:05:18 +0300

    add Comparsion class

---
+++ lessons/src/l014/comparsion/Comparsion.java
@@ -0,0 +1,16 @@
+package l014.comparsion;
+
+public class Comparsion {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 7;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
