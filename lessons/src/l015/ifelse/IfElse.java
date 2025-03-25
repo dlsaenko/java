a4a39ca8bf5bc251d018ed111ec4f29dfd65b89d
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-25 12:21:13 +0300

    add IfElse class

---
+++ lessons/src/l015/ifelse/IfElse.java
@@ -0,0 +1,20 @@
+package l015.ifelse;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        System.out.println("Введите 2 числа для сравнения:");
+        int a = new Scanner(System.in).nextInt();
+        int b = new Scanner(System.in).nextInt();
+
+        if (a > b) {
+            System.out.println(a + " больше " + b);
+        } else if (a < b) {
+            System.out.println(a + " меньше " + b);
+        } else {
+            System.out.println(a + " равно " + b);
+        }
+    }
+}
