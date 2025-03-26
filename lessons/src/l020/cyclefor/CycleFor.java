2b176e340866fa8470ee13f6679622842e36d027
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-26 19:38:43 +0300

    add CycleFor class

---
+++ lessons/src/l020/cyclefor/CycleFor.java
@@ -0,0 +1,10 @@
+package l020.cyclefor;
+
+public class CycleFor {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
