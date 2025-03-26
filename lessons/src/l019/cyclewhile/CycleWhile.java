d2296366faddeb16897a0dc611a59bd3a0541c33
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-26 16:22:05 +0300

    add CycleWhile class

---
+++ lessons/src/l019/cyclewhile/CycleWhile.java
@@ -0,0 +1,39 @@
+package l019.cyclewhile;
+
+import java.util.Scanner;
+
+public class CycleWhile {
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Введите номер дня недели:");
+            int a = new Scanner(System.in).nextInt();
+            switch (a) {
+                case 1:
+                    System.out.println("Понедельник");
+                    break;
+                case 2:
+                    System.out.println("Вторник");
+                    break;
+                case 3:
+                    System.out.println("Среда");
+                    break;
+                case 4:
+                    System.out.println("Четверг");
+                    break;
+                case 5:
+                    System.out.println("Пятница");
+                    break;
+                case 6:
+                    System.out.println("Суббота");
+                    break;
+                case 7:
+                    System.out.println("Воскресенье");
+                    break;
+
+                default:
+                    System.out.println("Такого дня недели не существует");
+            }
+        }
+    }
+}
