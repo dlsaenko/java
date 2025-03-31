45169b9a64b52ed7b206e08d709f5a988b66762f
dlsaenko <saenkodanilyurievich@mail.ru>
2025-03-31 13:35:43 +0300

    add InfiniteWhile class

---
+++ src/javacore/l025/infinitewhile/InfiniteWhile.java
@@ -0,0 +1,12 @@
+package javacore.l025.infinitewhile;
+
+public class InfiniteWhile {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (true) {
+            System.out.println("Hello - " + i++);
+        }
+    }
+}
