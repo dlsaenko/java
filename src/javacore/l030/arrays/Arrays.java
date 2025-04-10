4db775342aea70f1838841cdda58bf896ea52cd4
dlsaenko <saenkodanilyurievich@mail.ru>
2025-04-10 18:53:22 +0300

    add Arrays class

---
+++ src/javacore/l030/arrays/Arrays.java
@@ -0,0 +1,10 @@
+package javacore.l030.arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int[] array = {52, 3, 18, 9, 74};
+        String[] stringArray = {"string1", "test_text", "123", "Java"};
+        boolean[] boolArray = {true, false, false, true, true};
+    }
+}
