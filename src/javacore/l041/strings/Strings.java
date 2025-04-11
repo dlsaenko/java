bae978ccbde7ea6ae195ac86baee9f1bc7eaaf61
dlsaenko <saenkodanilyurievich@mail.ru>
2025-04-11 13:38:44 +0300

    add Strings class

---
+++ src/javacore/l041/strings/Strings.java
@@ -0,0 +1,16 @@
+package javacore.l041.strings;
+
+import java.util.Arrays;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        String str_1 = "Hello World!";
+        System.out.println(str_1);
+
+        System.out.println("Hello World!");
+
+        char[] str_2 = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+        System.out.println(Arrays.toString(str_2));
+    }
+}
